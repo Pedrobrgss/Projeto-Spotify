e9fd29db7ed3e2a8e65c71ea50dec3cac1c33a1b
Pedrobrgss <pedrobrgss1@gmail.com>
2025-02-14 13:31:43 -0300

    finalizamos o projeto

---
+++ back-end/api/connect.js
@@ -0,0 +1,7 @@
+import {MongoClient} from "mongodb"
+
+const URI = "mongodb+srv://pymongo:Pedro12272@cluster0.cmbmf5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const client = new MongoClient(URI)
+
+export const db = client.db("projetoSpotify")
---
+++ front-end/src/App2.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+//rafce
+
+//Arrow function
+//const App2 = () => <h1> App2 </h1>;
+
+//nomeação de componente 
+//PascalCase
+
+// Nomeação de classes em css
+// metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
