69d59e99d8c6bb52e55efcadf2f857586d4bb7c7
Pedrobrgss <pedrobrgss1@gmail.com>
2025-02-14 14:07:25 -0300

    Atualizando reporsitorio

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,9 +1,6 @@
 import {MongoClient} from "mongodb"
-import dotenv from "dotenv"
 
-dotenv.config();
-
-const URI = process.env.URI
+const URI = "mongodb+srv://pymongo:Pedro12272@cluster0.cmbmf5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(URI)
 
 export const db = client.db("projetoSpotify")
